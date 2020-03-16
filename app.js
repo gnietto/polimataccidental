033e968b66f8b83e4f2c17a8ec1bd52dd96dacdf
gnietto <47994713+gnietto@users.noreply.github.com>
2020-03-16 16:57:02 -0300

    actualiza pkgs, remueve logger por obsoleto

--- app.js
+++ app.js
@@ -8,22 +8,22 @@ function compile(str, path) {
 	return stylus(str)
 		.set('filename', path)
 		.use(nib())
-}
+};
 
 app.set('views', __dirname + '/views');
-app.set('view engine', 'pug')
-app.use(express.logger('dev'))
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/public'));
 app.use(stylus.middleware(
 		{
 			src: __dirname + '/public',
 			compile: compile
 		}
 	)
-)
-app.use(express.static(__dirname + '/public'))
+);
 
 app.get('/', function (req, res) {
 	res.render('index', {title: 'Home'})
-})
+});
 
 app.listen(3000);
