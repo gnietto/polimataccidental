6f909f9cf13b74de5bb8013eba131fd48fd9ae00
Administrador <elcirculoti@gmail.com>
2020-03-20 17:35:34 -0300

    agrega capacidad multicore a app, prepara package.json para heroku, modifica layout.pug

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
 const express = require('express');
 const stylus = require('stylus');
 const nib = require('nib');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const isDev = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 5000;
 
-const app = express();
 
-function compile(str, path) {
-	return stylus(str)
-		.set('filename', path)
-		.use(nib())
-};
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
+} else {
+  const app = express();
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'pug');
+	function compile(str, path) {
+		return stylus(str)
+			.set('filename', path)
+			.use(nib())
+	};
 
-app.use(stylus.middleware(
-		{
-			src: __dirname + '/public',
-			compile: compile
-		}
-	)
-);
-app.use(express.static(__dirname + '/public'));
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'pug');
 
-app.get('/', function (req, res) {
-	res.render('index', {title: 'Home'})
-});
+	app.use(stylus.middleware(
+			{
+				src: __dirname + '/public',
+				compile: compile
+			}
+		)
+	);
+	app.use(express.static(__dirname + '/public'));
 
-app.listen(3000);
+	app.get('/', function (req, res) {
+		res.render('index', {title: 'Inicio'})
+	});
+
+	app.listen(PORT);
+}
