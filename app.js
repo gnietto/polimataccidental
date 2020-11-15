67c21d1c5d53900fcdda853a185ab03c405d699f
workstation <elcirculoti@gmail.com>
2020-11-15 16:53:20 -0300

    agrega más info a post arboldelavida, agrega estilos y corrige post covid19

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ if (!isDev && cluster.isMaster) {
   		handle: 'acerca-del-covid19',
   		titulo: 'Acerca del Covid19',
       autor: 'Polímata Accidental',
-  		descripcion: 'Evidencia general acerca del Covid19',
+  		descripcion: 'Evidencia general acerca de la enfermedad Covid19',
   		fecha: '10 Noviembre 2020',
       tags: 'biología'
   	},
@@ -32,7 +32,7 @@ if (!isDev && cluster.isMaster) {
   		handle: 'el-arbol-de-la-vida',
   		titulo: 'El Árbol de la Vida',
       autor: 'Polímata Accidental',
-  		descripcion: 'Resumen de la diversidad biológica',
+  		descripcion: 'Resumen de la diversidad biológica de nuestro planeta',
   		fecha: '15 Noviembre 2020',
       tags: 'biología'
   	}
