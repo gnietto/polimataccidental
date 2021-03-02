6b0f1774fc9220eaae560a2a54864b86abe0bec0
workstation <elcirculoti@gmail.com>
2021-03-02 16:34:21 -0300

    separa datos de estructura

---
+++ public/handles/handles.js
@@ -0,0 +1,12 @@
+const posts = [    
+	{
+		handle: 'arbolvida',
+		titulo: 'El Árbol de la Vida',
+    autor: 'The Cybernetist',
+		descripcion: '¿Conoces la diversidad biológica de nuestro planeta?',
+		fecha: '15 Noviembre 2020',
+    tags: ['biología']
+	}  	
+];
+
+module.exports = posts;
