8c3480c227979fab387a5f9a30cd7a978afc1e2d
workstation <elcirculoti@gmail.com>
2021-03-03 15:24:42 -0300

    modifica longitud de texto descriptivo del post

--- public/handles/handles.js
+++ public/handles/handles.js
@@ -3,10 +3,10 @@ const posts = [
 		handle: 'arbolvida',
 		titulo: 'El Árbol de la Vida',
     autor: 'The Cybernetist',
-		descripcion: '¿Conoces la diversidad biológica de nuestro planeta?',
-		fecha: '15 Noviembre 2020',
+		descripcion: 'El rápido surgimiento de la enfermedad Covid19 en este 2020 me hace recordar que siempre me ha interesado la variedad de vida presente en nuestro planeta Tierra y la posibilidad cierta de vida en otros planetas o dimensiones. En este post busco mostrar las formas de vida conocidas...',
+		fecha: '05 Marzo 2021',
     tags: ['biología']
-	}  	
+	}, 	
 ];
 
 module.exports = posts;
